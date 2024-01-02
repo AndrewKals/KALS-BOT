643639d6501263b3e6b12208aff7d7eb62ae2a36
AndrewKals <155413156+AndrewKals@users.noreply.github.com>
2024-01-02 14:52:39 +0900

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['6281247087572', 'Kals', true],
+  ['6285374127751', 'Kals BOT', true], 
   [''] 
 ] //Number of owners
 
